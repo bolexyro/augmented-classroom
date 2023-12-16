d3ab47db815516914b7f53a6d38d54ff4be44b2a
Adebola Odufuwa <113560716+adebola-duf@users.noreply.github.com>
2023-12-16 14:32:55 +0100

    Allowed requests from all origins

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origin],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
